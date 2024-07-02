e06b9ff1a1a860ddc573c035741a83a473d8c6c2
muhammed abo lila <mabolila638@gmail.com>
2024-07-02 13:38:01 +0300

    redesign with tailwind 6.1-skills component

--- src/pages/SkillsPage/SkillsPage.jsx
+++ src/pages/SkillsPage/SkillsPage.jsx
@@ -5,77 +5,49 @@ const SkillsPage = ({ loading }) => {
   return (
     <>
       {loading === false ? (
-        <Container>
-          <section className="skills">
-            <div className="skills-content">
-              <Row>
-                <Col
-                  xs={{ span: 12 }}
-                  md={{ span: 6 }}
-                  lg={{ span: 3 }}
-                  className="p-0"
-                >
-                  <div className="one-skill">
-                    <h3>programming</h3>
-                    <div className="one-skill-content">
-                      <div className="icon-html5"></div>
-                      <div className="icon-css3"></div>
-                      <div className="icon-javascript"></div>
-                      <div className="icon-sass"></div>
-                    </div>
-                  </div>
-                </Col>
-                <Col
-                  xs={{ span: 12 }}
-                  md={{ span: 6 }}
-                  lg={{ span: 3 }}
-                  className="p-0"
-                >
-                  <div className="one-skill">
-                    <h3>Libraries</h3>
-                    <div className="one-skill-content">
-                      <div className="icon-bootstrap"></div>
-                      <div className="icon-jquery"></div>
-                      <div className="icon-react"></div>
-                      <div className="icon-redux"></div>
-                    </div>
-                  </div>
-                </Col>
-                <Col
-                  xs={{ span: 12 }}
-                  md={{ span: 6 }}
-                  lg={{ span: 3 }}
-                  className="p-0"
-                >
-                  <div className="one-skill">
-                    <h3>Tools</h3>
-                    <div className="one-skill-content">
-                      <div className="icon-visualstudiocode"></div>
-                      <div className="icon-postman"></div>
-                      <div className="icon-git"></div>
-                      <div className="icon-github"></div>
-                    </div>
-                  </div>
-                </Col>
-                <Col
-                  xs={{ span: 12 }}
-                  md={{ span: 6 }}
-                  lg={{ span: 3 }}
-                  className="p-0"
-                >
-                  <div className="one-skill personal">
-                    <h3>personal</h3>
-                    <ol className="one-skill-content">
-                      <li>Fast Learning Ability</li>
-                      <li>Time Management</li>
-                      <li>Communication Skills</li>
-                    </ol>
-                  </div>
-                </Col>
-              </Row>
+        <section className="w-[95%] sm:w-[90%] 2xl:w-[1320px] mx-auto h-full mb-20 mt-12 flex items-center justify-center lg:h-screen lg:my-0">
+          <div className="flex flex-wrap justify-between items-start w-full bg-mainBgColor border border-subColor rounded-lg overflow-hidden">
+
+            <div className="one-skill py-10 text-center border-l border-subColor">
+              <h3 className="text-[1.6rem] font-semibold py-4 px-5 relative text-grayColor capitalize after:content-[''] after:h-[1px] after:w-3/5 after:bg-subColor after:absolute after:top-full after:left-[20%] after:right-[20%]">programming</h3>
+              <div className="one-skill-content flex flex-wrap justify-center items-center mt-5">
+                <div className="icon-html5 w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-css3 w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-javascript w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-sass w-1/2 p-2 text-[2.9rem]"></div>
+              </div>
             </div>
-          </section>
-        </Container>
+
+            <div className="one-skill py-10 text-center border-l border-subColor">
+              <h3 className="text-[1.6rem] font-semibold py-4 px-5 relative text-grayColor capitalize after:content-[''] after:h-[1px] after:w-3/5 after:bg-subColor after:absolute after:top-full after:left-[20%] after:right-[20%]">Libraries</h3>
+              <div className="one-skill-content flex flex-wrap justify-center items-center mt-5">
+                <div className="icon-bootstrap w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-jquery w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-react w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-redux w-1/2 p-2 text-[2.9rem]"></div>
+              </div>
+            </div>
+
+            <div className="one-skill py-10 text-center border-l border-subColor">
+              <h3 className="text-[1.6rem] font-semibold py-4 px-5 relative text-grayColor capitalize after:content-[''] after:h-[1px] after:w-3/5 after:bg-subColor after:absolute after:top-full after:left-[20%] after:right-[20%]">Tools</h3>
+              <div className="one-skill-content flex flex-wrap justify-center items-center mt-5">
+                <div className="icon-visualstudiocode w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-postman w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-git w-1/2 p-2 text-[2.9rem]"></div>
+                <div className="icon-github w-1/2 p-2 text-[2.9rem]"></div>
+              </div>
+            </div>
+
+            <div className="one-skill personal py-10 text-center border-l border-subColor h-full bg-yellow-500">
+              <h3 className="text-[1.6rem] font-semibold py-4 px-5 relative text-grayColor capitalize after:content-[''] after:h-[1px] after:w-3/5 after:bg-subColor after:absolute after:top-full after:left-[20%] after:right-[20%]">personal</h3>
+              <ol className="one-skill-content block mt-5 text-center text-grayColor">
+                <li className="pt-2 text-[1rem] mt-2 leading-4">Fast Learning Ability</li>
+                <li className="pt-2 text-[1rem] mt-2 leading-4">Time Management</li>
+                <li className="pt-2 text-[1rem] mt-2 leading-4">Communication Skills</li>
+              </ol>
+            </div>
+          </div>
+        </section>
       ) : (
         <Loading image={image} text="Skills" />
       )}
