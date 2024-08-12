c1724950e35526074c4c6e8ca42d583ead4e46dd
Muhammed Abo Lila <mabolila638@gmail.com>
2024-08-13 00:38:58 +0300

    redesign with tailwind 7.2 experiences Component

--- src/pages/ExperiencesPage/ExperiencesPage.jsx
+++ src/pages/ExperiencesPage/ExperiencesPage.jsx
@@ -8,15 +8,15 @@ const ExperiencesPage = ({ loading }) => {
     slidesToShow: 2,
     slidesToScroll: 1,
     infinite: true,
-    autoplay: true,
-    speed: 15000,
-    autoplaySpeed: 1000,
-    pauseOnHover: true
+    // autoplay: true,
+    // speed: 15000,
+    // autoplaySpeed: 1000,
+    // pauseOnHover: true
   };
   return (
     <>
       {loading === false ? (
-        <section className="experience-container container h-full mb-20 mt-12 flex items-center justify-center md:h-screen md:my-0">
+        <section className="experience-container container h-screen mb-20 mt-12 flex flex-col items-center justify-center ">
           <div className="min-h-[400px] flex flex-wrap justify-center items-stretch w-full bg-mainBgColor border border-subColor rounded-lg overflow-hidden">
             <div className="experience-image bg-no-repeat bg-cover bg-center w-1/3 relative min-h-[inherit]">
               <a
@@ -28,30 +28,8 @@ const ExperiencesPage = ({ loading }) => {
                 Download CV
               </a>
             </div>
-            <Slider {...settings} className="w-2/3 ">
-              <div className="pt-1 px-2 bg-yellow-400 before:content-[ ] before:w-[1px] before:bg-subColor before:absolute before:top-0 before:bottom-0 before:left-0">
-                <div className="text-right font-medium flex flex-col items-end justify-center gap-1">
-                  <h3 className="text-[1.5rem] text-mainColor">
-                    Front-End Developer
-                  </h3>
-                  <p>Freelancer</p>
-                  <div className="mt-2">11/2022 - 6/2023</div>
-                  <div className="h-[1px] w-full bg-subColor mt-2"></div>
-                </div>
-                <ul className="pt-3 px-[20px] list-disc flex flex-col gap-3">
-                  <li>
-                    Collaborated closely with UX/UI designers to translate their
-                    creative vision into reality by designing and developing
-                    three engaging and visually appealing landing pages.
-                  </li>
-                  <li>
-                    Dedicated effort to enhance website performance,
-                    guaranteeing swift load times and seamless operation,
-                    thereby delivering an improved user experience
-                  </li>
-                </ul>
-              </div>
-              <div className="pt-1 px-2 bg-yellow-400 before:content-[ ] before:w-[1px] before:bg-subColor before:absolute before:top-0 before:bottom-0 before:left-0">
+            <Slider {...settings} className="w-2/3">
+              <div className="h-full relative pt-1 px-2 before:content-[ ] before:w-[1px] before:bg-subColor before:absolute before:top-0 before:bottom-0 before:left-0">
                 <div className="text-right font-medium flex flex-col items-end justify-center gap-1">
                   <h3 className="text-[1.5rem] text-mainColor">
                     Front-End Developer
@@ -73,7 +51,7 @@ const ExperiencesPage = ({ loading }) => {
                   </li>
                 </ul>
               </div>
-              <div className="pt-1 px-2 bg-yellow-400 before:content-[ ] before:w-[1px] before:bg-subColor before:absolute before:top-0 before:bottom-0 before:left-0">
+              <div className="h-full relative pt-1 px-2 before:content-[ ] before:w-[1px] before:bg-subColor before:absolute before:top-0 before:bottom-0 before:left-0">
                 <div className="text-right font-medium flex flex-col items-end justify-center gap-1">
                   <h3 className="text-[1.5rem] text-mainColor">
                     Front-End Developer
